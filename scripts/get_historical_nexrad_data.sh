44884d93d2f235489abf1db4b11ee11eccaa402e
DANRAMAGE <dan@inlet.geol.sc.edu>
2019-04-22 14:31:31 -0400

    Init checkin.

--- scripts/get_historical_nexrad_data.sh
+++ scripts/get_historical_nexrad_data.sh
@@ -2,4 +2,4 @@
 
 source /usr/local/virtualenv/pyenv-2.7.11/bin/activate;
 
-python /home/xeniaprod/scripts/Folly-Beach-WaterQuality/commonfiles/python/wqXMRGProcessing.py --ConfigFile=/home/xeniaprod/scripts/Folly-Beach-WaterQuality/config/folly_historical_build_config_debug.ini --ImportData=/mnt/historical_xmrg/2009
+python /home/xeniaprod/scripts/Folly-Beach-WaterQuality/commonfiles/python/wqXMRGProcessing.py --ConfigFile=/home/xeniaprod/scripts/Folly-Beach-WaterQuality/config/folly_historical_build_config.ini --ImportData=/mnt/historical_xmrg/2009
