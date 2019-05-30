fd56b38785d4dadc87c5c943cf1b46dcac0f1ded
DANRAMAGE <dan@inlet.geol.sc.edu>
2019-05-30 13:49:40 -0400

    Init checkin.

--- scripts/follybeach_wq_prediction_engine.sh
+++ scripts/follybeach_wq_prediction_engine.sh
@@ -4,4 +4,4 @@ source /usr/local/virtualenv/pyenv-2.7.11/bin/activate
 
 cd /home/xeniaprod/scripts/FollyBeach-WaterQuality/scripts;
 
-python follybeach_wq_prediction_engine.py --ConfigFile=/home/xeniaprod/scripts/Folly-Beach-WaterQuality/config/follybeach_prediction_engine.ini >> /home/xeniaprod/tmp/log/follybeach_wq_prediction_engine_sh.log 2>&1
+python follybeach_wq_prediction_engine.py --ConfigFile=/home/xeniaprod/scripts/FollyBeach-WaterQuality/config/folly_prediction_engine.ini >> /home/xeniaprod/tmp/log/follybeach_wq_prediction_engine_sh.log 2>&1
