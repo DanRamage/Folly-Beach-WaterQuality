bc5bd07e35f240775ef8f2a7109d069aa5387592
DANRAMAGE <dan@inlet.geol.sc.edu>
2019-06-04 14:02:39 -0400

    Init checkin.

--- scripts/data_collector_plugins/dhec_sample_data_collector_plugin.py
+++ scripts/data_collector_plugins/dhec_sample_data_collector_plugin.py
@@ -9,7 +9,8 @@ import geojson
 from datetime import datetime
 import time
 from pytz import timezone
-
+import json
+from shutil import copyfile
 
 from dhecBeachAdvisoryReader import waterQualityAdvisory
 from wq_output_results import wq_sample_data,wq_samples_collection,wq_advisories_file,wq_station_advisories_file
@@ -52,6 +53,12 @@ class dhec_sample_data_collector_plugin(data_collector_plugin):
 
       #Filepath to the geoJSON file that contains the station data for all the stations.
       stationGeoJsonFile = self._plugin_details.get('stationData', 'stationGeoJsonFile')
+
+      #Charleston sites to include. We have a list of the charleston site name then ";" then the Folly SIte name to use.
+      charleston_sites = self._plugin_details.get('stationData', 'sitesFromCharlestonToInclude').split(',')
+
+      charleston_directory = self._plugin_details.get('stationData', 'charlestonSampleDataDirectory')
+      charleston_predictions = self._plugin_details.get('stationData', 'charlestonPredictionFile')
 
       #The past WQ results.
       stationWQHistoryFile = self._plugin_details.get('stationData', 'stationWQHistoryFile')
@@ -108,14 +115,38 @@ class dhec_sample_data_collector_plugin(data_collector_plugin):
         utc_tz = timezone('UTC')
 
         for station in station_list:
-          station_data = data[station]
-          for rec in station_data['results']:
-            wq_sample = wq_sample_data()
-            wq_sample.station = station
-            date_obj = (utc_tz.localize(datetime.strptime(rec['date'], '%Y-%m-%dT%H:%M:%SZ'))).astimezone(est_tz)
-            wq_sample.date_time = date_obj
-            wq_sample.value = rec['value']
-            wq_data_collection.append(wq_sample)
+          if station in data:
+            station_data = data[station]
+            for rec in station_data['results']:
+              wq_sample = wq_sample_data()
+              wq_sample.station = station
+              date_obj = (utc_tz.localize(datetime.strptime(rec['date'], '%Y-%m-%dT%H:%M:%SZ'))).astimezone(est_tz)
+              wq_sample.date_time = date_obj
+              wq_sample.value = rec['value']
+              wq_data_collection.append(wq_sample)
+
+        #Now get the charleston wq project sites we want to include.
+        for chs_site in charleston_sites:
+          #Get the sample data.
+          try:
+            chs_site_name, folly_site_name = chs_site.split(';')
+            #First we copy the water quality sample data over to the FOlly BEach directory.
+            full_path = os.path.join(charleston_directory, "%s.json" % (chs_site_name))
+            to_path = os.path.join(json_output_path, "%s.json" % (folly_site_name))
+            copyfile(full_path, to_path)
+            #Now get the most recent sample date.
+            with open(to_path, "r") as chs_site_file:
+              json_data = json.load(chs_site_file)
+              recent = json_data['properties']['test']['beachadvisories'][-1]
+              wq_sample = wq_sample_data()
+              wq_sample.station = folly_site_name
+              date_obj = (utc_tz.localize(datetime.strptime(recent['date'], '%Y-%m-%d %H:%M:%S'))).astimezone(est_tz)
+              wq_sample.date_time = date_obj
+              wq_sample.value = recent['value']
+              wq_data_collection.append(wq_sample)
+          except (IOError, Exception) as e:
+            logger.exception(e)
+
 
         json_results_file = os.path.join(json_output_path, 'beach_advisories.json')
         logger.debug("Creating beach advisories file: %s" % (json_results_file))
@@ -126,23 +157,13 @@ class dhec_sample_data_collector_plugin(data_collector_plugin):
           logger.exception(e)
         try:
           for site in wq_sites:
-            logger.debug("Creating site: %s advisories file" % (site.name))
-            site_advisories = wq_station_advisories_file(site)
-            site_advisories.create_file(json_output_path, wq_data_collection)
+            if site.run_model:
+              logger.debug("Creating site: %s advisories file" % (site.name))
+              site_advisories = wq_station_advisories_file(site)
+              site_advisories.create_file(json_output_path, wq_data_collection)
         except Exception as e:
           logger.exception(e)
 
-        '''
-        logger.debug("Beginning SOAP query.")
-        advisoryObj.processData(
-                                geo_json_file = stationGeoJsonFile,
-                                json_file_path = jsonFilepath,
-                                historical_wq = historyWQ,
-                                dhec_url = dhec_rest_url,
-                                post_data_url = None,
-                                sampling_stations=wq_sites)
-        logger.debug("Finished SOAP query.")
-        '''
       except (IOError,Exception) as e:
         logger.exception(e)
 
