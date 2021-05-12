d86b865c67981c75fc4ce0b818c56f93962aa51b
DANRAMAGE <dan@inlet.geol.sc.edu>
2021-05-12 09:43:39 -0400

    Fixing up for Python3.

--- scripts/follybeach_ripcurrent_scraper.sh
+++ scripts/follybeach_ripcurrent_scraper.sh
@@ -5,3 +5,5 @@ source /usr/local/virtualenv/pyenv-3.8.5/bin/activate
 cd /home/xeniaprod/scripts/FollyBeach-WaterQuality/scripts;
 
 python FollyRipCurrentProcess.py --ConfigFile=/home/xeniaprod/scripts/FollyBeach-WaterQuality/config/FollyRipCurrent.ini > /home/xeniaprod/tmp/log/follybeach_ripcurrent_scraper_sh.log 2>&1
+
+python FollyRipCurrentProcess.py --ConfigFile=/home/xeniaprod/scripts/FollyBeach-WaterQuality/config/SarasotaRipCurrent.ini > /home/xeniaprod/tmp/log/follybeach_ripcurrent_scraper_sh.log 2>&1
